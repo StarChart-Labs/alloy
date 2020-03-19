5653bdb6728922ebceb3bc6f457ae80e13875125
Ryan O'Meara <romeara@live.com>
2020-03-19 00:50:57 -0400

    Resolves GH-65: Add Checkstyle validation of copyright headers
    Add a Checkstyle rule to ensure all java files have the correct
    copyright headers, and fix the files whose headers were inconsistent

--- alloy-core/src/main/java/org/starchartlabs/alloy/core/collections/package-info.java
+++ alloy-core/src/main/java/org/starchartlabs/alloy/core/collections/package-info.java
@@ -1,9 +1,10 @@
 /*
- * Copyright (C) 2018 StarChart Labs Authors.
+ * Copyright (C) 2018 StarChart-Labs@github.com Authors
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
  */
+
 /**
  * Utilities for interacting with common Java collection constructs
  *
